0fd279bb29d86c8e15ea94b89915447396d98b1f
TTREEz <81577691+TTREEz@users.noreply.github.com>
2025-04-15 19:46:17 -0400

    Update db.js

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ connection.connect((err) => {
     console.error('Error connecting to MySQL:', err);
     return;
   }
-  console.log('✅ Connected to MySQL Database');
+  console.log('Connected to MySQL Database');
 });
 
 module.exports = connection;
